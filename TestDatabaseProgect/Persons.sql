3d55fd03c076ded919f111d853fde0a91237dab6
Vladimir Vdovin <Wdowin@mail.ru>
2019-06-18 15:38:35 +0300

    #TDP-1 Добавление таблицы Persons
    Добавление файла Persons.sql, с таблицей [dbo]
    .[Persons].  Таблица предназначена
    для хранения списка физ лиц

---
+++ TestDatabaseProgect/Persons.sql
@@ -0,0 +1,34 @@
+﻿CREATE TABLE [dbo].[Persons]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Surname] NVARCHAR(50) NULL, 
+    [Name] NVARCHAR(50) NULL
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Идентификатор лица',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Persons',
+    @level2type = N'COLUMN',
+    @level2name = N'Id'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Фамилия',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Persons',
+    @level2type = N'COLUMN',
+    @level2name = N'Surname'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Имя',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Persons',
+    @level2type = N'COLUMN',
+    @level2name = N'Name'
